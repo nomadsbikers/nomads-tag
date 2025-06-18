c4644eda4876ca2c1c09a60f5fabcfefc87c9b60
nomadsbikers <nomadsbikers@outlook.com>
2025-06-18 00:35:12 -0600

    Update nomads_tag_prototipo.py

--- nomads_tag_prototipo.py
+++ nomads_tag_prototipo.py
@@ -61,3 +61,8 @@ def status(tag_id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+    import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
